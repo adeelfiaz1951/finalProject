09807776d94783ecd07f9a4ae8359021289e2cb4
Adeel Fiaz <122649696+adeelfiaz1951@users.noreply.github.com>
2025-11-24 16:29:34 +0100

    con interfaz y tkinter funcion pero hay errors main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from dataManager import crea_tablas
 import tkinter as tk
 from interface.login_UI import Login_ui
@@ -8,17 +8,3 @@ if __name__ == '__main__':
     root = tk.Tk()
     Login_ui(root)
     root.mainloop()
-=======
-# Importamos los módulos necesarios
-from biblio_dialog import BibliotecaApp
-
-# -------------------------------
-#        EJECUCIÓN PRINCIPAL
-# -------------------------------
-
-if __name__ == "__main__":
-    app = BibliotecaApp()
-    app.protocol("WM_DELETE_WINDOW", app.on_closing)
-    app.mainloop()
-
->>>>>>> 848af4fb687f16f569bf97e1b7f09164c57f8572
